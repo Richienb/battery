36afa138a1d7b57ff9189eb1fbb6998bd76181ad
Richie Bendall <richiebendall@gmail.com>
2020-05-30 18:34:36 +1200

    Update tests
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -5,6 +5,8 @@ const battery = require(".")
 test("main", async t => {
 	const { level, charging } = await battery()
 
-	t.true(typeof level === "number" && inRange(level, { start: 0, end: 1 }))
-	t.true(typeof charging === "boolean")
+	t.is(typeof level, "number")
+	t.true(inRange(level, { start: 0, end: 1 }))
+
+	t.is(typeof charging, "boolean")
 })
