8757c73222e2db53983a0ad83f40e15bc0d1e8b1
Richie Bendall <richiebendall@gmail.com>
2020-05-30 17:30:12 +1200

    Fix windows battery bug
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
 const getBatteryLevel = require("battery-level")
 const getBatteryCharging = require("is-charging")
 
-module.exports = async () => ({
-	level: await getBatteryLevel() || 1,
-	charging: await getBatteryCharging() || true
-})
+module.exports = async () => {
+	const level = await getBatteryLevel()
+	const charging = await getBatteryCharging()
+
+	// Windows doesn't support desktops which don't have batteries
+	if (Number.isNaN(level)) {
+		level = 1
+		charging = true
+	}
+
+	return {
+		level,
+		charging
+	}
+}
